247568081c40aa4176e3392f35433c5d62156011
Alex <alex.gurianov.2007@gmail.com>
2025-09-15 21:01:32 +0300

    Add func.h and func.cpp realisation

--- lab2/Friendly function and operator overload/func.h
+++ lab2/Friendly function and operator overload/func.h
@@ -1,9 +1,27 @@
-#ifndef FUNC
-#define FUNC
+#ifndef FUNC_H
+#define FUNC_H
 
-#include<iostream>
+#include <cctype>
+#include <iostream>
 
+class String {
+ private:
+  char* data;
+  size_t length;
 
+ public:
+  String(const String& other);
+  String();
+  String(const char* newString);
 
+  String& operator++();
+  String& operator--();
+  String& operator=(const String& other);
+
+  ~String();
+
+  friend void printString(const String& str, std::ostream& outputStream);
+  friend void inputString(String& str, std::istream& inputStream);
+};
 
 #endif
--- lab2/Friendly function and operator overload/main.cpp
+++ lab2/Friendly function and operator overload/main.cpp
@@ -1,6 +1,3 @@
-#include"func.h"
+#include "func.h"
 
-int main() {
-
-	return 0;
-}
+int main() { return 0; }
