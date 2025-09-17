869e50afa7189dddf378c19b2febef0c76e61274
Alex <alex.gurianov.2007@gmail.com>
2025-09-17 20:27:03 +0300

    format and fixed menu

--- lab2/Friendly function and operator overload/main.cpp
+++ lab2/Friendly function and operator overload/main.cpp
@@ -3,44 +3,44 @@
 using namespace std;
 
 int main() {
-    bool running = true;
+  bool running = true;
 
-    char choice;
+  char choice;
 
-    String myString;
+  String myString;
 
-    while (running) {
-        cout << "---------MENU---------" << endl;
-        cout << "1.Input string" << endl;
-        cout << "2.Print string" << endl;
-        cout << "3.Coding string" << endl;
-        cout << "4.Decoding string" << endl;
-        cout << "0. Exit" << endl;
+  while (running) {
+    cout << "---------MENU---------" << endl;
+    cout << "1.Input string" << endl;
+    cout << "2.Print string" << endl;
+    cout << "3.Coding string" << endl;
+    cout << "4.Decoding string" << endl;
+    cout << "0. Exit" << endl;
 
-        cin >> choice;
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin >> choice;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
-        switch (choice) {
-        case '1':
-            inputString(myString);
-            break;
-        case '2':
-            printString(myString);
-            break;
-        case '3':
-            ++myString;
-            break;
-        case '4':
-            --myString;
-            break;
-        case '0':
-            running = false;
-            cout << "Exit" << endl;
-            break;
-        default:
-            cout << "Error" << endl;
-        }
+    switch (choice) {
+      case '1':
+        inputString(myString);
+        break;
+      case '2':
+        printString(myString);
+        break;
+      case '3':
+        ++myString;
+        break;
+      case '4':
+        --myString;
+        break;
+      case '0':
+        running = false;
+        cout << "Exit" << endl;
+        break;
+      default:
+        cout << "Error" << endl;
     }
+  }
 
-    return 0;
+  return 0;
 }
